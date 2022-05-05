d71c7d44692f2e4d6a11bfa9ded0296edf4c46c2
anverabdullayev <anverabdullayev@gmail.com>
2022-05-05 18:32:52 +0400

    Implement API repositorie and controller

--- src/Services/Discount/Discount.API/Entities/Coupon.cs
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -2,7 +2,7 @@
 {
     public class Coupon
     {
-        public int Is { get; set; }
+        public int Id { get; set; }
         public string ProductName { get; set; }
         public string Description { get; set; }
         public int Amount { get; set; }
