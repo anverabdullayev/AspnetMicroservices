6375ad8d9722f256d798bb43b5eb63337f370297
anverabdullayev <anverabdullayev@gmail.com>
2022-05-19 12:22:44 +0400

    Implement Domain and Application layers of Ordering MS

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _ordersRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository ordersRepository, IMapper mapper)
+        {
+            _ordersRepository = ordersRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var ordersList = await _ordersRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(ordersList);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
